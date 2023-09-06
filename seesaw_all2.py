e9ce309e9754eab49fb77ef0b5a2b8ad3f9659a4
Koprvhdix <koprvhdix@gmail.com>
2023-09-06 15:49:06 +0800

    add seesaw_all

---
+++ seesaw_all2.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn.functional as F
+
+n = 4
+n_points = 300
+
+L_1_list = list()
+L_2_list = list()
+
+exchange_1 = [[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
+exchange_1 = torch.tensor(exchange_1)
+
+exchange_2 = [[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
+exchange_2 = torch.tensor(exchange_2)
+
+I_matrix = torch.eye(16) / 16
+
+L = torch.tril(torch.randn(16, 16), diagonal=-1)
+L = L + torch.diag(torch.exp(torch.randn(16)))
+L.requires_grad_(True)
+
+for i in range(n_points):
+    L_1 = torch.tril(torch.randn(n, n), diagonal=-1)
+    L_1 = L_1 + torch.diag(torch.exp(torch.randn(n)))
+    L_1.requires_grad_(True)
+    L_1_list.append(L_1)
+
+    L_2 = torch.tril(torch.randn(n, n), diagonal=-1)
+    L_2 = L_2 + torch.diag(torch.exp(torch.randn(n)))
+    L_2.requires_grad_(True)
+    L_2_list.append(L_2)
+
+weights = torch.randn(n_points)
+opti_list = L_1_list + L_2_list
+opti_list.append(weights)
+optimizer = torch.optim.SGD(opti_list, lr=0.0001)
+
+optimizer_2 = torch.optim.SGD([L], lr=0.0001)
+
+def train(train_optimizer, epoch, second_epoch):
+    P = L @ L.t()
+    Rho = P / P.trace()
+
+    # 计算半正定矩阵
+    L_1 = L_1_list[0] @ L_1_list[0].t()
+    L_2 = L_2_list[0] @ L_2_list[0].t()
+    L_1 = L_1 / L_1.trace()
+    L_2 = L_2 / L_2.trace()
+
+    target = (torch.flatten(torch.kron(L_1, L_2))).unsqueeze(0)
+    weights_normalized = F.normalize(torch.abs(weights), p=1, dim=0)
+
+    for j in range(1, n_points):
+        L_1 = L_1_list[j] @ L_1_list[j].t()
+        L_2 = L_2_list[j] @ L_2_list[j].t()
+        L_1 = L_1 / L_1.trace()
+        L_2 = L_2 / L_2.trace()
+        if j % 3 == 0:
+            target = torch.cat((target, (torch.flatten(torch.kron(L_1, L_2))).unsqueeze(0)), dim=0)
+        elif j % 3 == 1:
+            target = torch.cat(
+                (target, (torch.flatten(exchange_1 * torch.kron(L_1, L_2) * exchange_1)).unsqueeze(0)), dim=0)
+        else:
+            target = torch.cat(
+                (target, (torch.flatten(exchange_2 * torch.kron(L_1, L_2) * exchange_2)).unsqueeze(0)), dim=0)
+
+    target_matrix = torch.matmul(weights_normalized, target)
+    loss = -((torch.flatten(Rho) - target_matrix).pow(2).sum().sqrt())
+
+    train_optimizer.zero_grad()
+    loss.backward()
+    train_optimizer.step()
+
+    print(f'Epoch {epoch}: {second_epoch} Loss = {loss.item()}')
+
+for epoch in range(20):
+    P = L @ L.t()
+    Rho = P / P.trace()
+    for epoch1 in range(1000):
+        train(optimizer, epoch, epoch1)
+
+    for epoch2 in range(1000):
+        train(optimizer_2, epoch, epoch2)
+
